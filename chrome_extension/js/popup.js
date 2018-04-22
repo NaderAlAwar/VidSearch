c0e38e6e8372482c00315ed1f9369a8dfe59ed9e
NaderAlAwar <naderalawar@gmail.com>
2018-04-22 18:28:49 +0300

    added download functionality

--- chrome_extension/js/popup.js
+++ chrome_extension/js/popup.js
@@ -10,30 +10,37 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
-var url; 
+var url;
+var id;
 
 $( document ).ready(function() {  
-	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    	url = tabs[0].url;
-    	let URLdisplay = document.getElementById('textDiv');
-    	var checkURL = url.search("watch");						//checks if URL contains "watch", if the user has opened a youtube video
-    	if(checkURL == -1){										//if the user has not opened a youtube video
-    		URLdisplay.innerHTML = "Click on a youtube video to begin your search";
-    	}
-    	else{
-    		var id = url.replace("https://www.youtube.com/watch?v=", "");		//get the youtube video id
-    		var videoInformation = $.getJSON('https://noembed.com/embed',
-   				{format: 'json', url: url}, function (data) {
-   				URLdisplay.innerHTML = data.title;
-			});
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      url = tabs[0].url;
+      let URLdisplay = document.getElementById('textDiv');
+      var checkURL = url.search("watch");           //checks if URL contains "watch", if the user has opened a youtube video
+      if(checkURL == -1){                   //if the user has not opened a youtube video
+        URLdisplay.innerHTML = "Click on a youtube video to begin your search";
+      }
+      else{
+        id = url.replace("https://www.youtube.com/watch?v=", "");   //get the youtube video id
+        var videoInformation = $.getJSON('https://noembed.com/embed',
+          {format: 'json', url: url}, function (data) {
+          URLdisplay.innerHTML = data.title;
+      });
         document.getElementById("downloadButton").disabled = false;
       }
-	});
+  });
 });
 
 //TODO: send request to download video to server, then wait for confirmation
 
+$('#downloadButton').click(function(){
+  $.get("http://127.0.0.1:5000/downloadVideo/"+id, function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+    document.getElementById("downloadButton").disabled = true;
+  });
+});
 
 $('#searchButton').click(function(){
-	
+  
 });
